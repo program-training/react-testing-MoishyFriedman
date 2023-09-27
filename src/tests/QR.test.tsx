148710e9b3254e5292244b8a57888368dfbb21b0
moishy friedman <moshehlf@gmail.com>
2023-09-27 16:37:19 +0300

    more tests and fix bug

--- src/tests/QR.test.tsx
+++ src/tests/QR.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import { QR } from "../components/QR/QR";
+import { userEvent } from "@testing-library/user-event";
 
+const user = userEvent.setup();
 const details = {
   url: "https://github.com/testing-library/jest-dom#custom-matchers",
   time: new Date().toLocaleString(),
@@ -11,5 +13,12 @@ describe("QR", () => {
     render(<QR url={details.url} time={details.time} />);
     const imageElement = screen.getByRole("img");
     expect(imageElement).toBeInTheDocument();
+  });
+  test("click to meta data", async () => {
+    render(<QR url={details.url} time={details.time} />);
+    const buttonElement = screen.getByRole("button");
+    await user.click(buttonElement);
+    const pElement = screen.getByText(/100x100/i);
+    expect(pElement).toBeInTheDocument();
   });
 });
--- src/tests/url.test.tsx
+++ src/tests/url.test.tsx
@@ -1,10 +1,26 @@
 import { render, screen } from "@testing-library/react";
 import { Url } from "../components/Url/url.tsx";
+import { userEvent } from "@testing-library/user-event";
+
+const user = userEvent.setup();
 
 describe("Url", () => {
   test("check input", async () => {
     render(<Url />);
     const inputElement = screen.getByPlaceholderText("enter url");
     expect(inputElement).toBeInTheDocument();
+  });
+  test("rendering qr on click", async () => {
+    render(<Url />);
+    const user = userEvent.setup();
+    const buttonElement = screen.getByRole("button");
+    const inputElement = screen.getByRole("textbox");
+    await user.type(
+      inputElement,
+      "https://drive.google.com/file/d/124lqnR8rv6PIBAls-fs7QwTY35qLPAf7/view"
+    );
+    await user.click(buttonElement);
+    const qr = screen.getByRole("img");
+    expect(qr).toBeInTheDocument();
   });
 });
